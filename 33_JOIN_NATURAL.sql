ef16e795eb5f4ab46bb9bcbd32261cb5bca87f12
Jazziel Fabrizzio <edwinagemiler17@gmail.com>
2023-01-26 15:54:42 -0500

    Se agrega otro consulta más

---
+++ 33_JOIN_NATURAL.sql
@@ -0,0 +1,22 @@
+-- NATURAL JOIN
+/* SELECT table1.column, table2.column
+FROM table1
+NATURAL JOIN table2;
+*/
+
+SELECT *
+FROM locations NATURAL JOIN countries;
+
+SELECT l.street_address, l.city, c.country_name
+FROM locations l, countries c
+WHERE l.country_id = c.country_id;
+
+SELECT *
+FROM jobs NATURAL JOIN countries;
+
+DESC jobs;
+DESC countries;
+
+SELECT employee_id, job_id, department_id,
+e.last_name, e.hire_date, jh.end_date
+FROM job_history jh  NATURAL JOIN employees e;
